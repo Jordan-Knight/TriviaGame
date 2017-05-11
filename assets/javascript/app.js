d51a4bf543e367971eaac62b443afcbe8a16cd24
Jordan Knight <jordanknight@Jordans-MacBook-Pro.local>
2017-05-11 18:22:23 -0500

    working the logic

---
+++ assets/javascript/app.js
@@ -0,0 +1,44 @@
+var counter = 0;
+var correct = 0;
+var incorrect = 0;
+
+
+var app = {
+    q1 : {
+        question : "question1",
+        answers : ["1","2","3","4"],
+        correctAnswer : 1,
+    },
+    q2 : {
+        question : "question2",
+        answers : ["1","2","3","4"],
+        correctAnswer : 1,
+    },
+    q3 : {
+        question : "question3",
+        answers : ["1","2","3","4"],
+        correctAnswer : 1,
+    },
+    q4 : {
+        question : "question4",
+        answers : ["1","2","3","4"],
+        correctAnswer : 1,
+    },
+    q5 : {
+        question : "question5",
+        answers : ["1","2","3","4"],
+        correctAnswer : 1,
+    },
+    startGame : function(){
+
+    },
+    timer : function(){
+        setInterval(function(){
+            var counter = 20;
+            for
+
+        }, 20000);
+
+    },
+
+}
